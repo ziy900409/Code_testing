9d08d5cc6a9b57d533e1b3666329f658d2430673
Yang Hsin <45969661+ziy900409@users.noreply.github.com>
2022-08-07 14:16:51 +0800

    Update AirPistal_Statistic.py

--- AirPistal/AirPistal_Statistic.py
+++ AirPistal/AirPistal_Statistic.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jul 12 10:58:20 2022
-
-@author: Hsin Yang
+version 2
+增加區分擊發前跟擊發後的標準差與位移變化
+@author: Hsin
 """
 
 import os
@@ -41,12 +42,18 @@ def Read_File(x, y, subfolder='None'):
 # -----------------code start--------------------------
 # -----------------code start--------------------------
 
-file_path = r"D:\NTSU\TenLab\Shooting\AirPistol\Gun motion data_for Entropy\02-General gun motion"
+file_path_1 = r"D:\NTSU\TenLab\Shooting\AirPistol\Gun motion data_for Entropy\02-General gun motion"
+file_path_2 = r"D:\NTSU\TenLab\Shooting\AirPistol\Gun motion data_for Entropy\01-Elite gun motion"
 
-file_list = Read_File(file_path, '.xlsx', subfolder=False)
+
+file_list = Read_File(file_path_2, '.xlsx', subfolder=False)
 columns_name = ['mean_std_x', 'mean_std_y', 'mean_std_z',
-                'sum_dis_x', 'sum_dis_y', 'sum_dis_z']
-data = np.zeros((len(file_list), 6))
+                'pre_mean_std_x', 'pre_mean_std_y', 'pre_mean_std_z',
+                'pos_mean_std_x', 'pos_mean_std_y', 'pos_mean_std_z',
+                'sum_dis_x', 'sum_dis_y', 'sum_dis_z',
+                'pre_sum_dis_x', 'pre_sum_dis_y', 'pre_sum_dis_z',
+                'pos_sum_dis_x', 'pos_sum_dis_y', 'pos_sum_dis_z']
+data = np.zeros((len(file_list), 18))
 data_file_name = list(np.zeros((len(file_list), 1)))
 
 for ii in range(len(file_list)):
@@ -62,6 +69,21 @@ for ii in range(len(file_list)):
     mean_std_x = np.mean(std_x)
     mean_std_y = np.mean(std_y)
     mean_std_z = np.mean(std_z)
+    # separate pre-shooting and pos-shooting
+    # pre-shooting
+    pre_std_x = np.std(x_data.iloc[0:240, :])
+    pre_std_y = np.std(y_data.iloc[0:240, :])
+    pre_std_z = np.std(z_data.iloc[0:240, :])
+    pre_mean_std_x = np.mean(pre_std_x)
+    pre_mean_std_y = np.mean(pre_std_y)
+    pre_mean_std_z = np.mean(pre_std_z)
+    # pos-shooting
+    pos_std_x = np.std(x_data.iloc[240:, :])
+    pos_std_y = np.std(y_data.iloc[240:, :])
+    pos_std_z = np.std(z_data.iloc[240:, :])
+    pos_mean_std_x = np.mean(pos_std_x)
+    pos_mean_std_y = np.mean(pos_std_y)
+    pos_mean_std_z = np.mean(pos_std_z)
     #caculate sum of distance
     dis_x = np.zeros(np.shape(x_data))
     dis_y = np.zeros(np.shape(x_data))
@@ -70,19 +92,33 @@ for ii in range(len(file_list)):
         dis_x[i, :] = x_data.iloc[i+1, :] - x_data.iloc[i, :]
         dis_y[i, :] = y_data.iloc[i+1, :] - y_data.iloc[i, :]
         dis_z[i, :] = z_data.iloc[i+1, :] - z_data.iloc[i, :]
-    
-    sum_dis_x = np.sum(np.abs(dis_x))/np.shape(x_data)[1]
-    sum_dis_y = np.sum(np.abs(dis_y))/np.shape(x_data)[1]
-    sum_dis_z = np.sum(np.abs(dis_z))/np.shape(x_data)[1]
+    # caculate sum distance of gun motion
+    sum_dis_x = (np.sum(np.abs(dis_x)))/np.shape(x_data)[1]
+    sum_dis_y = (np.sum(np.abs(dis_y)))/np.shape(x_data)[1]
+    sum_dis_z = (np.sum(np.abs(dis_z)))/np.shape(x_data)[1]
+    # separate pre-shooting and pos-shooting
+    # pre-shooting of gun motion
+    pre_sum_dis_x = (np.sum(np.abs(dis_x[0:239])))/np.shape(x_data)[1]
+    pre_sum_dis_y = (np.sum(np.abs(dis_y[0:239])))/np.shape(x_data)[1]
+    pre_sum_dis_z = (np.sum(np.abs(dis_z[0:239])))/np.shape(x_data)[1] 
+    # pos-shooting of gun motion
+    pos_sum_dis_x = (np.sum(np.abs(dis_x[240:-1])))/np.shape(x_data)[1]
+    pos_sum_dis_y = (np.sum(np.abs(dis_y[240:-1])))/np.shape(x_data)[1]
+    pos_sum_dis_z = (np.sum(np.abs(dis_z[240:-1])))/np.shape(x_data)[1]
     # writing data in the data matrix
     data[ii, :] = [mean_std_x, mean_std_y, mean_std_z,
-                   sum_dis_x, sum_dis_y, sum_dis_z]
+                   pre_mean_std_x, pre_mean_std_y, pre_mean_std_z,
+                   pos_mean_std_x, pos_mean_std_y, pos_mean_std_z,
+                   sum_dis_x, sum_dis_y, sum_dis_z,
+                   pre_sum_dis_x, pre_sum_dis_y, pre_sum_dis_z,
+                   pos_sum_dis_x, pos_sum_dis_y, pos_sum_dis_z
+                   ]
     data_file_name[ii] = [file_list[ii]]
 
 data = pd.DataFrame(data, columns=columns_name)
 data_file_name = pd.DataFrame(data_file_name, columns=['file_name'])
 # write data to excel
-slove_path = r"D:\NTSU\TenLab\Shooting\AirPistol\Gun motion data_for Entropy\General_output.xlsx"
+slove_path = r"D:\NTSU\TenLab\Shooting\AirPistol\Gun motion data_for Entropy\Elite_output.xlsx"
 writer = pd.ExcelWriter(slove_path, engine='xlsxwriter')
 
 # Write each dataframe to a different worksheet.
